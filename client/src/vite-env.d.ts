ceb7c5c1d06a9081fa5d34a7da887edc97b007c6
Jackson Werner <74575896+cheeem@users.noreply.github.com>
2023-11-03 01:00:47 -0500

    Delete client/src/vite-env.d.ts

--- client/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="svelte" />
-/// <reference types="vite/client" />
-
-declare namespace svelteHTML {
-    interface HTMLAttributes<T> {
-        'on:view'?: (event: CustomEvent) => any;
-        'on:exit'?: (event: CustomEvent) => any;
-    }
-}
